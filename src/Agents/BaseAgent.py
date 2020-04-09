40790f6569bf82dec3757b7b487a1a271dd4569f
hwang-ua <han8@ualberta.ca>
2020-04-09 14:55:28 -0600

    Add function in Base classes

--- src/Agents/BaseAgent.py
+++ src/Agents/BaseAgent.py
@@ -1,4 +1,7 @@
 class BaseAgent:
+	def set_param(self, param):
+		raise NotImplementedError('Expected `set_param` to be implemented')
+
 	def start(self, observation):
 		raise NotImplementedError('Expected `start` to be implemented')
 
--- src/Environments/BaseEnvironment.py
+++ src/Environments/BaseEnvironment.py
@@ -1,9 +1,18 @@
 class BaseEnvironment:
-	def init(self, params={}):
-		raise NotImplementedError('Expected `init` to be implemented')
+	def set_param(self, param):
+		raise NotImplementedError('Expected `set_param` to be implemented')
 
 	def start(self):
 		raise NotImplementedError('Expected `init` to be implemented')
 
 	def step(self, action):
 		raise NotImplementedError('Expected `init` to be implemented')
+
+	def num_action(self):
+		raise NotImplementedError('Expected `num_action` to be implemented')
+
+	def state_dim(self):
+		raise NotImplementedError('Expected `state_dim` to be implemented')
+
+	def state_range(self):
+		raise NotImplementedError('Expected `state_range` to be implemented')
