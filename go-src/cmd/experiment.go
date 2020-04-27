8dfa7f212b58e9fcc33f5be9ec36068c1d171e9e
James Bell <james@bellstone.ca>
2020-04-27 16:31:10 -0600

    Move config parsing into Experiment

--- go-src/cmd/experiment.go
+++ go-src/cmd/experiment.go
@@ -1,27 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"io/ioutil"
 
 	_ "github.com/stellentus/cartpoles/go-src/lib/example"
 	"github.com/stellentus/cartpoles/go-src/lib/experiment"
-	"github.com/stellentus/cartpoles/go-src/lib/logger"
 	_ "github.com/stellentus/cartpoles/go-src/lib/remote"
-	"github.com/stellentus/cartpoles/go-src/lib/rlglue"
 )
 
 // Flags
 var (
 	configPath = flag.String("config", "config/example.json", "config file for the experiment")
 )
-
-type Config struct {
-	Environment rlglue.Attributes `json:"environment"`
-	Agent       rlglue.Attributes `json:"agent"`
-	Experiment  json.RawMessage   `json:"experiment"`
-}
 
 func main() {
 	flag.Parse()
@@ -31,27 +22,8 @@ func main() {
 		panic("The config file at path '" + *configPath + "' could not be read: " + err.Error())
 	}
 
-	var conf Config
-	err = json.Unmarshal(data, &conf)
-	if err != nil {
-		panic("The config file at path '" + *configPath + "' is not valid JSON: " + err.Error())
-	}
-
-	debugLogger := logger.NewDebug(logger.DebugConfig{
-		ShouldPrintDebug: true,
-		Interval:         2,
-	})
-	dataLogger := logger.NewData(debugLogger, logger.DataConfig{
-		ShouldLogTraces:         false,
-		ShouldLogEpisodeLengths: true,
-		NumberOfSteps:           1000,                     // TODO how to load this?
-		BasePath:                "/save/here/from/config", // TODO
-		FileSuffix:              "",                       // TODO after figuring out runs
-	})
-
-	expr, err := experiment.New(conf.Experiment, conf.Agent, conf.Environment, debugLogger, dataLogger)
+	err = experiment.Execute(data)
 	if err != nil {
 		panic("Could not create the experiment: " + err.Error())
 	}
-	expr.Run()
 }
--- go-src/lib/experiment/load.go
+++ go-src/lib/experiment/load.go
@@ -1,10 +1,49 @@
 package experiment
 
 import (
+	"encoding/json"
+	"errors"
+
 	"github.com/stellentus/cartpoles/go-src/lib/logger"
 	"github.com/stellentus/cartpoles/go-src/lib/registry"
 	"github.com/stellentus/cartpoles/go-src/lib/rlglue"
 )
+
+func Execute(data []byte) error {
+	var conf Config
+	err := json.Unmarshal(data, &conf)
+	if err != nil {
+		return errors.New("The config file is not valid JSON: " + err.Error())
+	}
+
+	debugLogger := logger.NewDebug(logger.DebugConfig{
+		ShouldPrintDebug: true,
+		Interval:         2,
+	})
+	dataLogger := logger.NewData(debugLogger, logger.DataConfig{
+		ShouldLogTraces:         false,
+		ShouldLogEpisodeLengths: true,
+		NumberOfSteps:           1000,                     // TODO how to load this?
+		BasePath:                "/save/here/from/config", // TODO
+		FileSuffix:              "",                       // TODO after figuring out runs
+	})
+
+	expr, err := New(conf.Experiment, conf.Agent, conf.Environment, debugLogger, dataLogger)
+	if err != nil {
+		return err
+	}
+
+	expr.Run()
+	return nil
+}
+
+type Config struct {
+	EnvironmentName string            `json:"environment-name"`
+	AgentName       string            `json:"agent-name"`
+	Environment     rlglue.Attributes `json:"environment-settings"`
+	Agent           rlglue.Attributes `json:"agent-settings"`
+	Experiment      json.RawMessage   `json:"experiment-settings"`
+}
 
 func InitializeEnvironment(name string, attr rlglue.Attributes, debug logger.Debug) (rlglue.Environment, error) {
 	var err error
