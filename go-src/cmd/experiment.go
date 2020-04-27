fead56a7cb7345a11bc1c7d94d51045cf7506c6e
James Bell <james@bellstone.ca>
2020-04-27 12:40:43 -0600

    Create example config for experiment

--- go-src/cmd/experiment.go
+++ go-src/cmd/experiment.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"io/ioutil"
 
+	_ "github.com/stellentus/cartpoles/go-src/lib/example"
 	"github.com/stellentus/cartpoles/go-src/lib/experiment"
 	"github.com/stellentus/cartpoles/go-src/lib/logger"
 	"github.com/stellentus/cartpoles/go-src/lib/rlglue"
@@ -16,24 +17,26 @@ var (
 )
 
 type Config struct {
-	environment rlglue.Attributes
-	agent       rlglue.Attributes
-	experiment  json.RawMessage
+	Environment rlglue.Attributes `json:"environment"`
+	Agent       rlglue.Attributes `json:"agent"`
+	Experiment  json.RawMessage   `json:"experiment"`
 }
 
 func main() {
+	flag.Parse()
+
 	data, err := ioutil.ReadFile(*configPath)
 	if err != nil {
-		panic("The config file at path '" + *configPath + "' could not be read")
+		panic("The config file at path '" + *configPath + "' could not be read: " + err.Error())
 	}
 
 	var conf Config
 	err = json.Unmarshal(data, &conf)
 	if err != nil {
-		panic("The config file at path '" + *configPath + "' is not valid JSON")
+		panic("The config file at path '" + *configPath + "' is not valid JSON: " + err.Error())
 	}
 
 	logger := logger.New()
-	expr, err := experiment.New(conf.experiment, conf.agent, conf.environment, logger)
+	expr, err := experiment.New(conf.Experiment, conf.Agent, conf.Environment, logger)
 	expr.Run()
 }
