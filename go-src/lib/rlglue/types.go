287b7cc02fb1cdc04bdce35cf7e61eaa5923e397
James Bell <james@bellstone.ca>
2020-04-27 14:24:50 -0600

    Remove LoadSaver

--- go-src/lib/rlglue/types.go
+++ go-src/lib/rlglue/types.go
@@ -2,7 +2,6 @@ package rlglue
 
 import (
 	"encoding/json"
-	"io"
 )
 
 // Action is a single integer representing the action taken by the agent.
@@ -46,28 +45,4 @@ type Attributes json.RawMessage
 
 func (attr *Attributes) UnmarshalJSON(data []byte) error {
 	return json.Unmarshal(data, (*json.RawMessage)(attr))
-}
-
-// LoadSaver can be implemented by an Agent or Environment to save itself. If it's implemented, the
-// Experiment will call these APIs when the config requests them.
-type LoadSaver interface {
-	// Save stores anything needed to save the struct (including relevant Config passed via Initialize)
-	// into the provided Writer
-	Save(io.Writer)
-
-	// Load loads the struct from the provided Reader. Loading a saved struct must produce a struct
-	// that behaves identically to the original struct, but with the new Logger.
-	Load(io.Reader, Logger)
-}
-
-// FileLoadSaver can be implemented by an Agent or Environment to save itself. If it's implemented, the
-// Experiment will call these APIs when the config requests them.
-type FileLoadSaver interface {
-	// Save stores anything needed to save the struct (including relevant Config passed via Initialize)
-	// at the provided filepath.
-	Save(string)
-
-	// Load loads the struct from the provided filepath. Loading a saved struct must produce a struct
-	// that behaves identically to the original struct, but with the new Logger.
-	Load(string, Logger)
 }
