b375db25dc8b6665d43e28713afbc2fdfe3dac72
Matthias Stone <matthias@bellstone.ca>
2020-04-27 13:18:55 -0600

    Add client wrapper for gRPC environments

--- go-src/lib/remote/environment.go
+++ go-src/lib/remote/environment.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/stellentus/cartpoles/go-src/lib/rlglue"
+
+	"google.golang.org/grpc"
 )
 
 type environmentServer struct {
@@ -32,4 +34,39 @@ func (srv environmentServer) Step(ctx context.Context, in *Action) (*StepResult,
 func (srv environmentServer) GetAttributes(ctx context.Context, in *Empty) (*Attributes, error) {
 	attr := srv.env.GetAttributes()
 	return &Attributes{Attributes: string(attr)}, nil
+}
+
+type environmentWrapper struct {
+	client EnvironmentClient
+}
+
+func NewEnvironment(cc *grpc.ClientConn) rlglue.Environment {
+	return &environmentWrapper{NewEnvironmentClient(cc)}
+}
+
+func (env environmentWrapper) Initialize(attr rlglue.Attributes, logger rlglue.Logger) error {
+	ctx := context.Background()
+	_, err := env.client.Initialize(ctx, &Attributes{Attributes: string(attr)})
+	return err
+}
+
+// Start returns an initial observation.
+func (env environmentWrapper) Start() rlglue.State {
+	ctx := context.Background()
+	state, _ := env.client.Start(ctx, &Empty{})
+	return rlglue.State(state.Values)
+}
+
+// Step takes an action and provides the new observation, the resulting reward, and whether the state is terminal.
+func (env environmentWrapper) Step(action rlglue.Action) (rlglue.State, float64, bool) {
+	ctx := context.Background()
+	result, _ := env.client.Step(ctx, &Action{Action: uint64(action)})
+	return rlglue.State(result.State.Values), result.Reward, result.Terminal
+}
+
+// GetAttributes returns attributes for this environment.
+func (env environmentWrapper) GetAttributes() rlglue.Attributes {
+	ctx := context.Background()
+	attr, _ := env.client.GetAttributes(ctx, &Empty{})
+	return rlglue.Attributes(attr.Attributes)
 }
