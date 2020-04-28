214ed3d2e71bdaa45acd40ad46bbbff5bd192df2
James Bell <james@bellstone.ca>
2020-04-28 09:00:52 -0600

    Add IsEqual for State

--- go-src/lib/rlglue/types.go
+++ go-src/lib/rlglue/types.go
@@ -2,6 +2,7 @@ package rlglue
 
 import (
 	"encoding/json"
+	"math"
 )
 
 // Action is a single integer representing the action taken by the agent.
@@ -14,4 +15,21 @@ type Attributes json.RawMessage
 
 func (attr *Attributes) UnmarshalJSON(data []byte) error {
 	return json.Unmarshal(data, (*json.RawMessage)(attr))
+}
+
+const StatePrecision = 0.000001
+
+func (s State) IsEqual(s2 State) bool {
+	if s == nil {
+		return s2 == nil
+	}
+	if s2 == nil || len(s) != len(s2) {
+		return false
+	}
+	for i := range s {
+		if math.Abs(s[i]-s2[i]) > StatePrecision {
+			return false
+		}
+	}
+	return true
 }
