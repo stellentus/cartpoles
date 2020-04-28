5ae3e84a8c8da6984ef0cb7ae71c0bb6a7b86390
James Bell <james@bellstone.ca>
2020-04-28 10:51:20 -0600

    Add debug to example agent

--- go-src/lib/agent/example.go
+++ go-src/lib/agent/example.go
@@ -11,8 +11,10 @@ import (
 // Example just iterates through all actions, starting from a random one.
 type Example struct {
 	logger.Debug
-	lastAction      int
-	NumberOfActions int `json:"numberOfActions"`
+	lastAction          int
+	NumberOfActions     int `json:"numberOfActions"`
+	EnableDebug         bool
+	StateContainsReplay bool `json:"state-contains-replay"`
 }
 
 func init() {
@@ -25,12 +27,16 @@ func NewExample(logger logger.Debug) (rlglue.Agent, error) {
 
 // Initialize configures the agent with the provided parameters and resets any internal state.
 func (agent *Example) Initialize(expAttr, envAttr rlglue.Attributes) error {
-	var ss struct{ Seed int64 }
+	var ss struct {
+		Seed        int64
+		EnableDebug bool `json:"enable-debug"`
+	}
 	err := json.Unmarshal(expAttr, &ss)
 	if err != nil {
 		agent.Message("warning", "agent.Example seed wasn't available: "+err.Error())
 		ss.Seed = 0
 	}
+	agent.EnableDebug = ss.EnableDebug
 
 	err = json.Unmarshal(envAttr, &agent)
 	if err != nil {
@@ -40,14 +46,20 @@ func (agent *Example) Initialize(expAttr, envAttr rlglue.Attributes) error {
 	rng := rand.New(rand.NewSource(ss.Seed)) // Create a new rand source for reproducibility
 	agent.lastAction = rng.Intn(agent.NumberOfActions)
 
-	agent.Message("msg", "agent.Example Initialize", "seed", ss.Seed, "numberOfActions", agent.NumberOfActions)
+	if agent.EnableDebug {
+		agent.Message("msg", "agent.Example Initialize", "seed", ss.Seed, "numberOfActions", agent.NumberOfActions)
+	}
 
 	return nil
 }
 
 // Start provides an initial observation to the agent and returns the agent's action.
 func (agent *Example) Start(state rlglue.State) rlglue.Action {
-	return agent.Step(state, 0)
+	if agent.EnableDebug {
+		agent.Message("msg", "start")
+	}
+	act := agent.Step(state, 0)
+	return act
 }
 
 // Step provides a new observation and a reward to the agent and returns the agent's next action.
@@ -56,8 +68,20 @@ func (agent *Example) Step(state rlglue.State, reward float64) rlglue.Action {
 	if agent.lastAction > agent.NumberOfActions {
 		agent.lastAction = 0
 	}
-	return rlglue.Action(agent.lastAction)
+	act := rlglue.Action(agent.lastAction)
+	if agent.EnableDebug {
+		if agent.StateContainsReplay {
+			agent.Message("msg", "step", "state", state[0], "reward", reward, "action", act, "expected action", state[1])
+		} else {
+			agent.Message("msg", "step", "state", state, "reward", reward, "action", act)
+		}
+	}
+	return act
 }
 
 // End informs the agent that a terminal state has been reached, providing the final reward.
-func (agent *Example) End(state rlglue.State, reward float64) {}
+func (agent *Example) End(state rlglue.State, reward float64) {
+	if agent.EnableDebug {
+		agent.Message("msg", "end", "state", state, "reward", reward)
+	}
+}
