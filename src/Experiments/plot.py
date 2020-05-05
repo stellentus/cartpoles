a237f3eac923a96422ba0d30c8ccaa9255873ba6
hwang-ua <han8@ualberta.ca>
2020-05-05 11:13:28 -0600

    plotting code

--- src/Experiments/plot.py
+++ src/Experiments/plot.py
@@ -135,7 +135,7 @@ def control_exp_single_env(env_name, result_path, handcode=None, eval=False):
         #         handcode_data = None
 
         # The following block plots the number of failure cases
-        elif env_name.lower() in ["ccp"]:
+        elif env_name.lower() in ["ccp", "ccp-sarsa", "ccp-sarsa-model", "ccp-dqn", "ccp-dqn-model"]:
             ignore_zero = False
             exp_smooth = None
             best = "smallAUC"
@@ -148,15 +148,25 @@ def control_exp_single_env(env_name, result_path, handcode=None, eval=False):
             if handcode:
                 handcode_data = accum_reward_all_runs(handcode, run_num, num_steps)
                 handcode_data *= -1 # number of failures = -1 * accumulate reward
+            else:
+                handcode_data = None
         else:
             raise NotImplemented
 
         if one_setting is not None:
             key = ''
-            if config.agent == 'DQN':
-                key = "B{}_sync{}".format(
+            if config.agent == "DQN":
+                key = "B{}_sync{}_batch{}".format(
                     folder.split("_B")[1].split("_")[0],
-                    folder.split("_sync")[1].split("_")[0]
+                    folder.split("_sync")[1].split("_")[0],
+                    folder.split("_batch")[1].split("_")[0]
+                )
+            elif config.agent == "ExpectedSarsaTileCodingContinuing":
+                key = "tiling{}_tile{}_lmbda{}_epsilon{}".format(
+                    folder.split("_TC")[1].split("x")[0],
+                    folder.split("_TC")[1].split("_")[0].split("x")[1],
+                    folder.split("_lmbda")[1].split("_")[0],
+                    folder.split("_epsilon")[1].split("_")[0],
                 )
             if env_params.drift_prob > 0:
                 key += "drift_scale{}_life{}_prob{}".format(
@@ -177,7 +187,53 @@ def control_exp_single_env(env_name, result_path, handcode=None, eval=False):
     save_path = "../data/plots/{}".format(env_name)
     pf.plot_control_exp_curve(all_data, label, lim_x, lim_y, ignore_zero=ignore_zero, exp_smooth=exp_smooth, save_path=None, handcode=handcode_data, best=best)
 
+def multiple_agent(path_list, run_num, num_steps):
+    all_data = {}
+    all_label = {}
+    for path, label in path_list:
+        one_setting = accum_reward_all_runs(path, run_num, num_steps)
+        one_setting *= -1
+        if label in all_data.keys():
+            all_data[label].append(one_setting)
+            all_label[label].append(label)
+        else:
+            all_data[label] = [one_setting]
+            all_label[label] = [label]
+    pf.plot_control_exp_curve(all_data, all_label, [0, num_steps], [-10, 90])
+
+def quick_test(path):
+    reward = np.load(path)
+    accum = np.zeros(len(reward))
+    sum = 0
+    for i in range(len(reward)):
+        sum += reward[i]
+        accum[i] = sum
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.plot(accum * -1)
+    plt.show()
+
+
 if __name__ == '__main__':
-    control_exp_single_env("CCP", "../data/exp_result",
-                           handcode="../data/exp_result/offline_eval/ContinuingCartpoleEnvironment_DQN_B500_sync10_NN[128, 128]_alpha1e-05_inputObs",
+    control_exp_single_env("ccp-sarsa-model", "../data/exp_result/simulatorTraining/handcode_pd8",
+                           handcode="../data/offline_env/test/HandCoded/",
                            eval=True)
+    control_exp_single_env("ccp-dqn-model", "../data/exp_result/simulatorTraining/handcode_pd8",
+                           handcode="../data/offline_env/test/HandCoded/",
+                           eval=True)
+    # control_exp_single_env("ccp-sarsa", "../data/exp_result/",
+    #                        handcode="../data/offline_env/test/HandCoded/",
+    #                        eval=True)
+    # control_exp_single_env("ccp-dqn", "../data/exp_result/",
+    #                        handcode="../data/offline_env/test/HandCoded/",
+    #                        eval=True)
+
+    # multiple_agent(
+    #     [["../data/exp_result/offlineTraining_simulatorEval/handcode_freq2/offline_eval/ContinuingCartpoleEnvironment_DQN_B500_sync25_NN[128, 128]_batch16_alpha0.001_inputObs_training10000", "DQN"],
+    #      ["../data/exp_result/offlineTraining_simulatorEval/handcode_freq2/offline_eval/ContinuingCartpoleEnvironment_ExpectedSarsaTileCodingContinuing_lmbda0.6_epsilon0.2_alpha0.1_inputSep_pair_TC32x2_training10000", "Sarsa"],
+    #      ["../data/offline_env/test/HandCoded/", "HandCoded"]],
+    #     10,
+    #     10000
+    # )
+
+    # quick_test("../data/offline_env/test/HandCoded_reward.npy")
