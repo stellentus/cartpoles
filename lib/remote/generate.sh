39eb6749d9915c09dbc6f946fa9627e17679b042
James Bell <james@bellstone.ca>
2020-05-11 14:51:26 -0600

    Add comment to generate.sh

--- lib/remote/generate.sh
+++ lib/remote/generate.sh
@@ -1,2 +1,3 @@
+# This script is used to re-generate the remote.pb.go and python files when remote.proto is updated.
 protoc -I . remote.proto --go_out=plugins=grpc:.
 python3 -m grpc_tools.protoc -I . remote.proto --python_out=. --grpc_python_out=.
