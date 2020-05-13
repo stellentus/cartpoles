73834fee5b74c096daf01350f962d64a28ca5502
James Bell <james@bellstone.ca>
2020-05-13 11:41:33 -0600

    Add template expected sarsa lambda

---
+++ lib/agent/esarsa.go
@@ -0,0 +1,80 @@
+package agent
+
+import (
+	"encoding/json"
+	"math"
+	"math/rand"
+
+	"github.com/stellentus/cartpoles/lib/logger"
+	"github.com/stellentus/cartpoles/lib/rlglue"
+	"github.com/stellentus/cartpoles/lib/util"
+)
+
+// Expected sarsa-lambda with tile coding
+type ESarsa struct {
+	logger.Debug
+	rng   *rand.Rand
+	tiler util.MultiTiler
+}
+
+func init() {
+	Add("esarsa-lambda", NewESarsa)
+}
+
+func NewESarsa(logger logger.Debug) (rlglue.Agent, error) {
+	return &ESarsa{Debug: logger}, nil
+}
+
+// Initialize configures the agent with the provided parameters and resets any internal state.
+func (agent *ESarsa) Initialize(run uint, expAttr, envAttr rlglue.Attributes) error {
+	var ss struct {
+		Seed        int64
+		EnableDebug bool `json:"enable-debug"`
+		NumTiles    int  `json:"tiles"`       // This was 32 in the python code
+		TileSpread  int  `json:"tile-spread"` // This was 4+1 in the python code
+	}
+	err := json.Unmarshal(expAttr, &ss)
+	if err != nil {
+		agent.Message("warning", "agent.ESarsa seed wasn't available: "+err.Error())
+		ss.Seed = 0
+	}
+
+	agent.rng = rand.New(rand.NewSource(ss.Seed + int64(run))) // Create a new rand source for reproducibility
+
+	scalers := []util.Scaler{
+		util.NewScaler(-2.4, 2.4, ss.TileSpread),
+		util.NewScaler(-4.0, 4.0, ss.TileSpread),
+		util.NewScaler(-(12 * 2 * math.Pi / 360), (12 * 2 * math.Pi / 360), ss.TileSpread),
+		util.NewScaler(-3.5, 3.5, ss.TileSpread),
+	}
+
+	agent.tiler, err = util.NewMultiTiler(4, ss.NumTiles, scalers)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Start provides an initial observation to the agent and returns the agent's action.
+func (agent *ESarsa) Start(state rlglue.State) rlglue.Action {
+	_, err := agent.tiler.Tile(state) // Rename the _ to whatever you want it to be. It's a slice of indices of tile activations
+	if err != nil {
+		agent.Message("err", "agent.ESarsa is acting on garbage state because it couldn't create tiles: "+err.Error())
+	}
+
+	panic("Not implemented")
+
+	return 0
+}
+
+// Step provides a new observation and a reward to the agent and returns the agent's next action.
+func (agent *ESarsa) Step(state rlglue.State, reward float64) rlglue.Action {
+	panic("Not implemented")
+	return 0
+}
+
+// End informs the agent that a terminal state has been reached, providing the final reward.
+func (agent *ESarsa) End(state rlglue.State, reward float64) {
+	panic("Not implemented")
+}
