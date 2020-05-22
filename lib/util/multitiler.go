a0bab9b5c93817e8981921956055e9f695782add
James Bell <james@bellstone.ca>
2020-05-22 15:08:38 -0600

    Fix bug in calculation of the number of indices

--- lib/util/multitiler.go
+++ lib/util/multitiler.go
@@ -13,8 +13,8 @@ type MultiTiler struct {
 	// scalers is an array of scaling information to be applied to each input data dimension.
 	scalers []Scaler
 
-	// numOutputs is the number of tile indicies that could be produced. Valid indices range between 0 and numOutputs-1.
-	numOutputs int
+	// numIndices is the number of tile indicies that could be produced. Valid indices range between 0 and numOutputs-1.
+	numIndices int
 }
 
 // NewMultiTiler creates a new Indexing Tiler, which returns a slice of indexes based on the tiles' hashes.
@@ -38,7 +38,7 @@ func NewMultiTiler(numDims, tiles int, scalers []Scaler) (MultiTiler, error) {
 		return MultiTiler{}, fmt.Errorf("Mismatch between len(Scaler) and the number of dimensions (%d != %d)", len(scalers), numDims)
 	}
 
-	numOutputs := 0
+	numIndices := 0
 
 	// Create tilers for each single dimension
 	singles, err := tile.NewSinglesTiler(numDims, tiles)
@@ -46,7 +46,7 @@ func NewMultiTiler(numDims, tiles int, scalers []Scaler) (MultiTiler, error) {
 		return MultiTiler{}, fmt.Errorf("Could not create aggregate tiler: %s", err.Error())
 	}
 	for _, scaler := range scalers {
-		numOutputs += scaler.MaxRange * tiles
+		numIndices += maxIndices(scaler.MaxRange, 1, tiles)
 	}
 
 	// Create tilers for each pair of dimensions
@@ -55,7 +55,7 @@ func NewMultiTiler(numDims, tiles int, scalers []Scaler) (MultiTiler, error) {
 		return MultiTiler{}, fmt.Errorf("Could not create aggregate tiler: %s", err.Error())
 	}
 	for _, scaler := range scalers {
-		numOutputs += scaler.MaxRange * tiles
+		numIndices += maxIndices(scaler.MaxRange, 2, tiles)
 	}
 
 	// Create a mega-tiler that appends pairs and singles
@@ -65,8 +65,8 @@ func NewMultiTiler(numDims, tiles int, scalers []Scaler) (MultiTiler, error) {
 	}
 
 	// Now change the tiler to return indexes instead of hashes
-	it, err := tile.NewIndexingTiler(til, numOutputs)
-	return MultiTiler{it: it, scalers: scalers}, err
+	it, err := tile.NewIndexingTiler(til, numIndices)
+	return MultiTiler{it: it, scalers: scalers, numIndices: numIndices}, err
 }
 
 // Tile returns a vector of length equal to tiles (the argument to NewMultiTiler). That vector contains indices
@@ -86,5 +86,30 @@ func (st MultiTiler) Tile(data []float64) ([]int, error) {
 }
 
 func (st MultiTiler) NumberOfIndices() int {
-	return st.numOutputs
+	return st.numIndices
+}
+
+// maxIndices returns the maximum number of indices with a given maximum range of input data, number of dimensions
+// in the input data, and number of tilings. It assumes the number of tilings is the same in each dimension.
+// For example, when tiling a 4-dimensional input, with each input value ranging from -3 to 6, and 32 tilings, the
+// call would be `maxIndices(6-(-3), 4, 32)`.
+func maxIndices(maxRange, numDims, numTilings int) int {
+	ranges := make([]int, numDims)
+	for i := range ranges {
+		ranges[i] = maxRange
+	}
+	return maxIndicesForRanges(ranges, numTilings)
+}
+
+// maxIndicesForRanges returns the maximum number of indices with a given maximum range of input data and number of
+// tilings. The ranges slice contains the maximum range for each of the input values being hashed.
+func maxIndicesForRanges(ranges []int, numTilings int) int {
+	result := 1
+	for _, val := range ranges {
+		// Add +1 because the most values along the "edges" of the region will include tiles that are otherwise entirely out of the region.
+		result *= val + 1
+	}
+
+	result *= numTilings
+	return result
 }
