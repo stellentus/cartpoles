23444983f4f3b79176f456c5c49c951d527905cd
James Bell <james@bellstone.ca>
2020-06-08 12:25:29 -0600

    Switch CSV loading to numpy.loadtxt
    * I did this to speed things up, but it's not really faster. The next commit should help.

--- plot/load.py
+++ plot/load.py
@@ -10,10 +10,9 @@ def load_data(alg_path):
 	returns_list_files = np.array([])
 
 	for file in rewards_files:
-		rewards_csv = csv.reader(open(alg_path+'/'+file))
-		next(rewards_csv) #skips the first line
+		f = open(alg_path+'/'+file)
+		rewards_list = np.loadtxt(f, skiprows=1).T # Import CSV and skip the header line
 
-		rewards_list = np.array([float(reward) for row in rewards_csv for reward in row])
 		returns_list = np.cumsum(rewards_list)
 
 		if returns_list_files.size != 0:
