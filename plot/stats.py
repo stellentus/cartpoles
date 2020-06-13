ff60e44d18941c278946084abe0361469b265933
Archit Sakhadeo <gemsbound007@users.noreply.github.com>
2020-06-12 21:24:19 -0600

    Add functions to calculate statistics

--- plot/stats.py
+++ plot/stats.py
@@ -1,46 +1,86 @@
 import numpy as np
 
-def getMeanAndCI95(returns_list_files):
-    numruns = returns_list_files.shape[0]
+# Returns the mean of the runs per timestep
+def getMean(data):
+    return np.mean(data, axis=0)
 
-    mean_returns_list = np.mean(returns_list_files, axis=0)
-    stddev_returns_list = np.std(returns_list_files, axis=0)
+# Returns the standard deviation of the runs per timestep
+def getStddev(data):
+    return np.std(data, axis=0)
 
-    CI95_returns_list = 1.96 * stddev_returns_list / np.sqrt(numruns)
-    min_CI95_returns_list = mean_returns_list - CI95_returns_list
-    max_CI95_returns_list = mean_returns_list + CI95_returns_list
+# Returns the median run
+def getMedian(data):
+    numRuns = len(data)
 
-    return mean_returns_list, min_CI95_returns_list, max_CI95_returns_list
+    # Sorts runs w.r.t. final performance in an ascending manner
+    indices = np.argsort(data[:,-1])
 
-
-'''
-Calculate the median run based on final performance
-'''
-def getMedianBestWorstFinal(returns_list_files):
-    numruns = returns_list_files.shape[0]
-
-    indices = np.argsort(returns_list_files[:,-1])
-    if numruns % 2 == 1:
-        median_returns_list = returns_list_files[indices[int((numruns-1)/2)]]
+    if numRuns % 2 == 1:
+        # Odd number of runs, select middle run
+        median = data[indices[int((numRuns-1)/2)]]
     else:
-        median_returns_list = (returns_list_files[indices[int(numruns/2)]] + returns_list_files[indices[int(numruns/2 - 1)]])/2.0
+        # Even number of runs, select average of the two runs in the middle
+        median = (data[indices[int(numRuns/2)]] + data[indices[int(numRuns/2 - 1)]])/2.0
+    return median
 
-    best_returns_list = returns_list_files[indices[-1]]
-    worst_returns_list = returns_list_files[indices[0]]
+# Returns the run with the best performance (highest return or least failures)
+def getBest(data, transformation='Returns'):
+    numRuns = len(data)
 
-    return median_returns_list, best_returns_list, worst_returns_list
+    # Sorts runs w.r.t. final performance in an ascending manner
+    indices = np.argsort(data[:,-1])
 
-
-'''
-Calculate median across every timestep, without reporting the median run
-'''
-def getMedianPerTimestep(returns_list_files):
-    numruns = returns_list_files.shape[0]
-
-    sorted_returns_list = np.sort(returns_list_files, axis=0)
-    if numruns % 2 == 1:
-        median_returns_list = sorted_returns_list[int((numruns-1)/2)]
+    # Change this code carefully
+    if transformation == 'Failures':
+        best = data[indices[0]]
     else:
-        median_returns_list = (sorted_returns_list[int(numruns/2)] + sorted_returns_list[int(numruns/2 - 1)]) / 2.0
+        best = data[indices[-1]]
+    return best
 
-    return median_returns_list
+# Returns the run with the worst performance (lowest return or most failures)
+def getWorst(data, transformation='Returns'):
+    numRuns = len(data)
+
+    # Sorts runs w.r.t. final performance in an ascending manner
+    indices = np.argsort(data[:,-1])
+
+    # Change this code carefully
+    if transformation == 'Failures':
+        worst = data[indices[-1]]
+    else:
+        worst = data[indices[0]]
+    return worst
+
+# Returns the confidence interval of the mean
+# Make this code adaptive using pandas/scipy so it finds the critical value given the confidence
+def getConfidenceIntervalOfMean(data, confidence=0.95):
+    numRuns = len(data)
+    mean = np.mean(data, axis=0)
+    stddev = np.std(data, axis=0)
+    criticalValue = 1.96
+    confidenceInterval = criticalValue * stddev / np.sqrt(numRuns)
+    lowerbound = mean - confidenceInterval
+    upperbound = mean + confidenceInterval
+    return lowerbound, upperbound
+
+# Sorts the performances in an ascending manner
+# Returns the lower and upper performances based on their percentile
+def getRegion(data, lower=0.0, upper=1.0, transformation='Returns'):
+    numRuns = len(data)
+
+    # Sorts runs w.r.t. final performance in an ascending manner
+    indices = np.argsort(data[:,-1])
+    lowerIndex = int(round( lower * ( numRuns-1 ) ))
+    upperIndex = int(round( upper * ( numRuns-1 ) ))
+
+    # Change this code carefully
+    if transformation == 'Failures':
+        # li and ui are important and not redundant. They act like temp variables
+        li = lowerIndex
+        ui = upperIndex
+        lowerIndex = numRuns - 1 - ui
+        upperIndex = numRuns - 1 - li
+        
+    lowerRun = data[indices[lowerIndex]]
+    upperRun = data[indices[upperIndex]]
+    return lowerRun, upperRun
