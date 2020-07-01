fa9592082947693e58b89d8c95cd5b2e4000a9e6
Archit Sakhadeo <gemsbound007@users.noreply.github.com>
2020-06-30 18:52:23 -0600

    Find best run from parameter sweep

---
+++ plot/findBestMean.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+
+algorithms = ['esarsa', 'dqn']
+basePath = '../data/'
+
+for i in range(len(algorithms)):
+	failuresList = []
+	parameterSettings = os.listdir(basePath + algorithms[i])
+	indexMinFailures = 0
+	minFailures = 0
+	for j in range(len(parameterSettings)):
+		if j % 10 == 0:
+			print(algorithms[i], j)
+
+		failures = 0
+		runs = os.listdir(basePath + algorithms[i] + '/' + parameterSettings[j])
+		count = 0
+		for k in range(len(runs)):
+			if 'episodes' in runs[k]:
+				count += 1
+
+				# subtract 1 for the first row
+				failures += int(subprocess.check_output('cat ' + basePath + algorithms[i] + '/' + parameterSettings[j] + '/' + runs[k] + ' | wc -l', shell=True)) - 1
+		failures /= count
+		
+		if len(failuresList) != 0:
+			if failures < minFailures:
+				indexMinFailures = j
+				minFailures = failures
+		else:
+			minFailures = failures
+
+		failuresList.append(failures)
+	
+	print('\nLeast number of failures for ' + algorithms[i] + ' is for the parameter setting: ' + parameterSettings[indexMinFailures])
+	print('Number of failures for that setting is: ', failuresList[indexMinFailures], '\n')
+
+
+
+
+
