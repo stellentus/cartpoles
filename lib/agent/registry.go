97142f81f973d4b6667a556e4a7467007ce80b1e
James Bell <james@bellstone.ca>
2020-08-19 17:11:07 -0600

    Use PersistentAgent in experiments
    * Save/Load aren't used yet. This just creates infrastructure to make sure that regular agents can be passed around as persistent agents, but they'll throw an error if their Save/Load is called.

--- lib/agent/registry.go
+++ lib/agent/registry.go
@@ -2,18 +2,19 @@ package agent
 
 import (
 	"errors"
+	"io"
 
 	"github.com/stellentus/cartpoles/lib/logger"
 	"github.com/stellentus/cartpoles/lib/rlglue"
 )
 
-// NewAgentCreator is a function that can create an agent.
-type NewAgentCreator func(logger.Debug) (rlglue.Agent, error)
+// NewPersistentAgentCreator is a function that can create an agent.
+type NewPersistentAgentCreator func(logger.Debug) (rlglue.PersistentAgent, error)
 
-// Add is used to register a new agent type.
-// This is most likely called by an init function in the Agent's go file.
-// The function returns an error if an agent with that name already exists.
-func Add(name string, creator NewAgentCreator) error {
+var agentList = map[string]NewPersistentAgentCreator{}
+
+// AddPersistent is used to register a new persistent agent type.
+func AddPersistent(name string, creator NewPersistentAgentCreator) error {
 	if _, ok := agentList[name]; ok {
 		return errors.New("Agent '" + name + "' has already been registered")
 	}
@@ -21,7 +22,8 @@ func Add(name string, creator NewAgentCreator) error {
 	return nil
 }
 
-func Create(name string, debug logger.Debug) (rlglue.Agent, error) {
+// Create creates an agent of the provided name.
+func Create(name string, debug logger.Debug) (rlglue.PersistentAgent, error) {
 	creator, ok := agentList[name]
 	if !ok {
 		return nil, errors.New("Agent '" + name + "' has not been registered")
@@ -29,4 +31,29 @@ func Create(name string, debug logger.Debug) (rlglue.Agent, error) {
 	return creator(debug)
 }
 
-var agentList = map[string]NewAgentCreator{}
+// NewAgentCreator is a function that can create an agent.
+// This is most likely called by an init function in the Agent's go file.
+// The function returns an error if an agent with that name already exists.
+type NewAgentCreator func(logger.Debug) (rlglue.Agent, error)
+
+// Add is used to register a new agent type.
+func Add(name string, creator NewAgentCreator) error {
+	persistentCreator := func(debug logger.Debug) (rlglue.PersistentAgent, error) {
+		agent, err := creator(debug)
+		return persistentAgent{Agent: agent}, err
+	}
+
+	return AddPersistent(name, persistentCreator)
+}
+
+type persistentAgent struct {
+	rlglue.Agent
+}
+
+func (pa persistentAgent) Save(wr io.Writer) error {
+	return errors.New("Attempt to call Save on rlglue.Agent")
+}
+
+func (pa persistentAgent) Load(rd io.Reader) error {
+	return errors.New("Attempt to call Load on rlglue.Agent")
+}
