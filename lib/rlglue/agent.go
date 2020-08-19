ed83eb11127439bdca00c0f7d326595437235b15
James Bell <james@bellstone.ca>
2020-08-19 17:09:54 -0600

    Create a PersistentAgent
    * This is an agent with a Save and a Load.

--- lib/rlglue/agent.go
+++ lib/rlglue/agent.go
@@ -1,5 +1,7 @@
 package rlglue
 
+import "io"
+
 type Agent interface {
 	// Initialize configures the agent with the provided parameters and resets any internal state.
 	// The run number is used in case many agents are run simultaneously, e.g. to modify the random seed.
@@ -14,4 +16,18 @@ type Agent interface {
 
 	// End informs the agent that a terminal state has been reached, providing the final reward.
 	End(state State, reward float64)
+}
+
+// PersistentAgent is an agent that can be persisted to a file. The data format is not specified.
+type PersistentAgent interface {
+	Agent
+
+	// Save saves the agent to the provided writer.
+	Save(wr io.Writer) error
+
+	// Load loads the agent from the provided reader. If used, it should only be called before
+	// the Agent's Initialize is called. If the experiment or environment attributes are persisted,
+	// Initialize might need to check if the loaded attributes match the ones provided to
+	// Initialize, or it might handle conflicts in some way.
+	Load(rd io.Reader) error
 }
