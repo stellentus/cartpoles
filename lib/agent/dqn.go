00ba42e4e787123c2b025994ad9b27d404742eb9
hwang-ua <han8@ualberta.ca>
2020-09-01 21:44:42 -0600

    control weight lock setting from json

--- lib/agent/dqn.go
+++ lib/agent/dqn.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"os"
 
 	ao "github.com/stellentus/cartpoles/lib/util/array-opr"
 	"github.com/stellentus/cartpoles/lib/util/network"
@@ -18,6 +19,7 @@ import (
 //type Model struct {
 //}
 type LockWeight struct {
+	UseLock		bool
 	DecCount	int
 	BestAvg 	float64
 	LockAvg		float64
@@ -28,6 +30,7 @@ func NewLockWeight() LockWeight {
 	lw.DecCount = 0
 	lw.BestAvg = math.Inf(-1)
 	lw.LockThrd = 2
+	lw.UseLock = false
 	return lw
 }
 
@@ -90,7 +93,38 @@ func NewDqn(logger logger.Debug) (rlglue.Agent, error) {
 }
 
 func (agent *Dqn) Initialize(run uint, expAttr, envAttr rlglue.Attributes) error {
-	err := json.Unmarshal(expAttr, &agent.dqnSettings)
+
+	var ss struct {
+		Seed        int64
+		EnableDebug bool `json:"enable-debug"`
+
+		NumberOfActions     int     `json:"numberOfActions"`
+		StateContainsReplay bool    `json:"state-contains-replay"`
+		Gamma               float64 `json:"gamma"`
+		Epsilon             float64 `json:"epsilon"`
+		MinEpsilon          float64 `json:"min-epsilon"`
+		DecreasingEpsilon   string  `json:"decreasing-epsilon"`
+
+		Hidden    []int   `json:"dqn-hidden"`
+		Alpha     float64 `json:"alpha"`
+		Sync      int     `json:"dqn-sync"`
+		Decay     float64 `json:"dqn-decay"`
+		Momentum  float64 `json:"dqn-momentum"`
+		AdamBeta1 float64 `json:"dqn-adamBeta1"`
+		AdamBeta2 float64 `json:"dqn-adamBeta2"`
+		AdamEps   float64 `json:"dqn-adamEps"`
+
+		Bsize int    `json:"buffer-size"`
+		Btype string `json:"buffer-type"`
+
+		StateDim  int `json:"state-len"`
+		BatchSize int `json:"dqn-batch"`
+
+		StateRange []float64 `json:"StateRange"`
+
+		UseLock   bool    `json:"lock-weight"`
+	}
+	err := json.Unmarshal(expAttr, &ss)
 	if err != nil {
 		return errors.New("DQN agent attributes were not valid: " + err.Error())
 	}
@@ -127,6 +161,7 @@ func (agent *Dqn) Initialize(run uint, expAttr, envAttr rlglue.Attributes) error
 
 	agent.lw = NewLockWeight()
 	agent.lock = false
+	agent.lw.UseLock = ss.UseLock
 
 	return nil
 }
@@ -194,13 +229,16 @@ func (agent *Dqn) Feed(lastS rlglue.State, lastA int, state rlglue.State, reward
 
 
 func (agent *Dqn) Update() {
-	//if agent.updateNum%agent.Bsize == 0 {
-	//	agent.lock = agent.CheckChange()
-	//}
-	//if agent.lock {
-	//	agent.updateNum += 1
-	//	return
-	//}
+	if agent.lw.UseLock {
+		os.Exit(1)
+		if agent.updateNum%agent.Bsize == 0 {
+			agent.lock = agent.CheckChange()
+		}
+		if agent.lock {
+			agent.updateNum += 1
+			return
+		}
+	}
 
 	if agent.updateNum%agent.Sync == 0 {
 		// NN: Synchronization
@@ -211,18 +249,12 @@ func (agent *Dqn) Update() {
 	}
 
 	lastStates, lastActions, states, rewards, gammas := agent.bf.Sample(agent.BatchSize)
-	//lastStates := ao.Index2d(samples, 0, len(samples), 0, agent.StateDim)
-	//lastActions := ao.Flatten2DInt(ao.A64ToInt2D(ao.Index2d(samples, 0, len(samples), agent.StateDim, agent.StateDim+1)))
-	//states := ao.Index2d(samples, 0, len(samples), agent.StateDim+1, agent.StateDim*2+1)
-	//rewards := ao.Flatten2DFloat(ao.Index2d(samples, 0, len(samples), agent.StateDim*2+1, agent.StateDim*2+2))
-	//gammas := ao.Flatten2DFloat(ao.Index2d(samples, 0, len(samples), agent.StateDim*2+2, agent.StateDim*2+3))
 
 	// NN: Weight update
 	lastQ := agent.learningNet.Forward(lastStates)
 	lastActionValue := ao.RowIndexFloat(lastQ, lastActions)
 	targetQ := agent.learningNet.Predict(states)
 	targetActionValue, _ := ao.RowIndexMax(targetQ)
-	//fmt.Println(targetActionValue[0])
 
 	loss := make([][]float64, len(lastQ))
 	for i := 0; i < len(lastQ); i++ {
@@ -270,19 +302,6 @@ func (agent *Dqn) Policy(state rlglue.State) int {
 		allValue := agent.learningNet.Predict(inputS)
 		_, idxs := ao.RowIndexMax(allValue)
 		idx = idxs[0]
-
-		//allValue := agent.learningNet.Predict(inputS)[0]
-		//var argmax int
-		//var v float64
-		//max := math.Inf(-1)
-		//for i := 0; i < len(allValue); i++ {
-		//	v = allValue[i]
-		//	if v > max {
-		//		max = v
-		//		argmax = i
-		//	}
-		//}
-		//idx = argmax
 
 	}
 	return idx
