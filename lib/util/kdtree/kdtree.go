ecdbcfbd74f1eb5bf2613aad0203223b5554fa93
hwang-ua <han8@ualberta.ca>
2020-09-09 00:51:59 -0600

    update code for build/search kdtree from logged data

--- lib/util/kdtree/kdtree.go
+++ lib/util/kdtree/kdtree.go
@@ -6,19 +6,19 @@ import (
 )
 
 type TransTrees struct {
-	 trees 		[]*kdtree.Tree
-	 data		[][][]float64
-	 count 		[]int
-	 numAction 	int
-	 stateDim 	int
-	 distanceFunc  	pairwise.PairwiseDistanceFunc
+	trees        []*kdtree.Tree
+	data         [][][]float64
+	count        []int
+	numAction    int
+	stateDim     int
+	distanceFunc pairwise.PairwiseDistanceFunc
 }
 
-func New(numAction int, actionIdx int, dist string) TransTrees{
+func New(numAction int, actionIdx int, dist string) TransTrees {
 	ts := make([]*kdtree.Tree, numAction)
 	data := make([][][]float64, numAction)
 	cs := make([]int, numAction)
-	for i:=0; i<numAction; i++ {
+	for i := 0; i < numAction; i++ {
 		ts[i] = kdtree.New()
 		cs[i] = 0
 	}
@@ -32,21 +32,23 @@ func New(numAction int, actionIdx int, dist string) TransTrees{
 	return t
 }
 
-func (t *TransTrees)BuildTree(allTrans [][]float64) {
+func (t *TransTrees) BuildTree(allTrans [][]float64) {
 	var action int
-	for i:=0; i<len(allTrans[0]); i++ {
+	dataInTree := make([][][]float64, t.numAction)
+	for i := 0; i < len(allTrans); i++ {
 		action = int(allTrans[i][t.stateDim])
-		t.data[action] = append(t.data[action], allTrans[i][: t.stateDim]) // sort current state
+		t.data[action] = append(t.data[action], allTrans[i])                  // sort current state
+		dataInTree[action] = append(dataInTree[action], allTrans[i][:t.stateDim]) // sort current state
 	}
-	for i:=0; i<t.numAction; i++ {
-		err := t.trees[i].Build(t.data[i])
+	for i := 0; i < t.numAction; i++ {
+		err := t.trees[i].Build(dataInTree[i])
 		if err != nil {
-			panic("Error when building tree for action "+string(i))
+			panic("Error when building tree: " + string(err.Error()))
 		}
 	}
 }
 
-func (t *TransTrees) SearchTree(target []float64, action int, k int) ([][]float64, [][]float64, []float64, []float64, []float64){
+func (t *TransTrees) SearchTree(target []float64, action int, k int) ([][]float64, [][]float64, []float64, []float64, []float64) {
 	neighborIdxs, dists, err := t.trees[action].Search(k, t.distanceFunc, target) // search by current state
 	if err != nil {
 		panic(err)
@@ -54,14 +56,14 @@ func (t *TransTrees) SearchTree(target []float64, action int, k int) ([][]float6
 	states := make([][]float64, k)
 	nextStates := make([][]float64, k)
 	rewards := make([]float64, k)
-	gammas := make([]float64, k)
-	for i:=0; i<k; i++ {
-		states[i] = t.data[action][neighborIdxs[i]][: t.stateDim]
-		nextStates[i] = t.data[action][neighborIdxs[i]][t.stateDim+1: t.stateDim*2+1]
+	terminals := make([]float64, k)
+	for i := 0; i < k; i++ {
+		states[i] = t.data[action][neighborIdxs[i]][:t.stateDim]
+		nextStates[i] = t.data[action][neighborIdxs[i]][t.stateDim+1 : t.stateDim*2+1]
 		rewards[i] = t.data[action][neighborIdxs[i]][t.stateDim*2+1]
-		gammas[i] = t.data[action][neighborIdxs[i]][t.stateDim*2+2]
+		terminals[i] = t.data[action][neighborIdxs[i]][t.stateDim*2+2]
 	}
-	return states, nextStates, rewards, gammas, dists
+	return states, nextStates, rewards, terminals, dists
 }
 
 //func test() {
