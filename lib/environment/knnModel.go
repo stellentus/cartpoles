6f00424a37154c9a55e390a7f7b23f07efce3282
hwang-ua <han8@ualberta.ca>
2020-09-09 00:51:00 -0600

    update code for build/search kdtree from logged data

--- lib/environment/knnModel.go
+++ lib/environment/knnModel.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/stellentus/cartpoles/lib/util/convformat"
 	transModel "github.com/stellentus/cartpoles/lib/util/kdtree"
 	"log"
 	"math/rand"
@@ -17,25 +18,22 @@ import (
 	"github.com/stellentus/cartpoles/lib/rlglue"
 )
 
-const (
-
-)
-
 type knnSettings struct {
-	Seed         int64     `json:"seed"`
-	offlineLog   string    `json:"trans-log"`
-	NumberOfActions     int     `json:"numberOfActions"`
-	stateRange   []float64 `json:"stateRange"`
+	DataLog string `json:"datalog"`
+	Seed    int64  `json:"seed"`
 }
 
 type KnnModelEnv struct {
 	logger.Debug
 	knnSettings
-	state             rlglue.State
-	rng               *rand.Rand
-	offlineData		  [][]float64
-	offlineModel	  transModel.TransTrees
-	stateDim		  int
+	state           rlglue.State
+	rng             *rand.Rand
+	offlineData     [][]float64
+	offlineStarts   []int
+	offlineModel    transModel.TransTrees
+	stateDim        int
+	NumberOfActions int
+	stateRange      []float64
 }
 
 func init() {
@@ -46,24 +44,7 @@ func NewKnnModelEnv(logger logger.Debug) (rlglue.Environment, error) {
 	return &KnnModelEnv{Debug: logger}, nil
 }
 
-// Initialize configures the environment with the provided parameters and resets any internal state.
-func (env *KnnModelEnv) Initialize(run uint, attr rlglue.Attributes) error {
-	err := json.Unmarshal(attr, &env.knnSettings)
-	if err != nil {
-		err = errors.New("environment.Cartpole settings error: " + err.Error())
-		env.Message("err", err)
-		return err
-	}
-	env.Seed += int64(run)
-	env.rng = rand.New(rand.NewSource(env.Seed)) // Create a new rand source for reproducibility
-	env.state = make(rlglue.State, 4)
-
-	env.Message("offline knn model environment settings", fmt.Sprintf("%+v", env.knnSettings))
-
-	folder := env.knnSettings.offlineLog
-	traceLog := folder+"/trace-"+string(run)+".csv"
-	paramLog := folder + "/log_json.txt"
-
+func (env *KnnModelEnv) SettingFromLog(paramLog string) {
 	// Get state dimension
 	txt, err := os.Open(paramLog)
 	if err != nil {
@@ -77,19 +58,14 @@ func (env *KnnModelEnv) Initialize(run uint, attr rlglue.Attributes) error {
 	var numAction int
 	var stateRange []float64
 	for scanner.Scan() {
-		fmt.Println()
 		line = scanner.Text()
 		spl = strings.Split(line, "=")
-		if spl[0] == "state-len" { //stateDimension
+		if spl[0] == "stateDimension" { //stateDimension
 			stateDim, _ = strconv.Atoi(spl[1])
 		} else if spl[0] == "numberOfActions" {
 			numAction, _ = strconv.Atoi(spl[1])
 		} else if spl[0] == "stateRange" {
-			strRange := strings.Split(spl[1][1:len(spl[1])-1], ", ")
-			stateRange = make([]float64, len(strRange))
-			for i:=0; i<len(strRange); i++ {
-				stateRange[i], _ = strconv.ParseFloat(strRange[i], 64)
-			}
+			stateRange = convformat.ListStr2Float(spl[1], ",")
 		}
 	}
 	if err := scanner.Err(); err != nil {
@@ -98,6 +74,26 @@ func (env *KnnModelEnv) Initialize(run uint, attr rlglue.Attributes) error {
 	env.stateDim = stateDim
 	env.NumberOfActions = numAction
 	env.stateRange = stateRange
+}
+
+// Initialize configures the environment with the provided parameters and resets any internal state.
+func (env *KnnModelEnv) Initialize(run uint, attr rlglue.Attributes) error {
+	err := json.Unmarshal(attr, &env.knnSettings)
+	if err != nil {
+		err = errors.New("environment.knnModel settings error: " + err.Error())
+		env.Message("err", err)
+		return err
+	}
+	env.Seed += int64(run)
+	env.rng = rand.New(rand.NewSource(env.Seed)) // Create a new rand source for reproducibility
+	env.state = make(rlglue.State, 4)
+
+	env.Message("environment.knnModel settings", fmt.Sprintf("%+v", env.knnSettings))
+
+	folder := env.knnSettings.DataLog
+	traceLog := folder + "/traces-" + strconv.Itoa(int(run)) + ".csv"
+	paramLog := folder + "/log_json.txt"
+	env.SettingFromLog(paramLog)
 
 	// Get offline data
 	csvFile, err := os.Open(traceLog)
@@ -109,48 +105,72 @@ func (env *KnnModelEnv) Initialize(run uint, attr rlglue.Attributes) error {
 	if err != nil {
 		log.Fatal(err)
 	}
-	allTrans := make([][]float64, len(allTransStr))
-	for i:=0; i<len(allTransStr); i++ {
+	allTrans := make([][]float64, len(allTransStr)-1)
+	for i := 1; i < len(allTransStr); i++ { // remove first str (title of column)
 		trans := allTransStr[i]
-		row := make([]float64, len(trans))
+		row := make([]float64, env.stateDim*2+3)
 		for j, num := range trans {
-			row[j], _ = strconv.ParseFloat(num, 64)
+			if j == 0 { // next state
+				num = num[1 : len(num)-1] // remove square brackets
+				copy(row[env.stateDim+1:env.stateDim*2+1], convformat.ListStr2Float(num, " "))
+			} else if j == 1 { // current state
+				num = num[1 : len(num)-1]
+				copy(row[:env.stateDim], convformat.ListStr2Float(num, " "))
+			} else if j == 2 { // action
+				row[env.stateDim], _ = strconv.ParseFloat(num, 64)
+			} else if j == 3 { //reward
+				row[env.stateDim*2+1], _ = strconv.ParseFloat(num, 64)
+				if row[env.stateDim*2+1] == -1 { // termination
+					row[env.stateDim*2+2] = 1
+				} else {
+					row[env.stateDim*2+2] = 0
+				}
+			}
 		}
-		allTrans[i] = row
+		allTrans[i-1] = row
 	}
 	env.offlineData = allTrans
-	env.offlineModel = transModel.New(numAction, stateDim, "euclidean")
+	env.offlineStarts = env.SearchOfflineStart(allTrans)
+	env.offlineModel = transModel.New(env.NumberOfActions, env.stateDim, "euclidean")
 	env.offlineModel.BuildTree(allTrans)
 	return nil
 }
 
+func (env *KnnModelEnv) SearchOfflineStart(allTrans [][]float64) []int {
+	starts := []int{0}
+	for i := 0; i < len(allTrans)-1; i++ { // not include the end of run
+		if allTrans[i][len(allTrans[i])-1] == 1 {
+			starts = append(starts, i+1)
+		}
+	}
+	return starts
+}
+
 func (env *KnnModelEnv) randomizeState() rlglue.State {
-	randIdx := env.rng.Int()
-	return env.offlineData[randIdx][:env.stateDim]
+	randIdx := env.rng.Intn(len(env.offlineStarts))
+	return env.offlineData[env.offlineStarts[randIdx]][:env.stateDim]
 }
 
 // Start returns an initial observation.
 func (env *KnnModelEnv) Start() rlglue.State {
 	env.state = env.randomizeState()
-	cp := make([]float64, env.stateDim)
-	copy(cp, env.state)
-	return cp
+	return env.state
 }
 
 func (env *KnnModelEnv) Step(act rlglue.Action) (rlglue.State, float64, bool) {
-	_, nextStates, rewards, gammas, _ := env.offlineModel.SearchTree(env.state, int(act), 1)
-	cp := make([]float64, env.stateDim)
-	env.state = nextStates[0]
-	copy(cp, env.state)
+	_, nextStates, rewards, terminals, _ := env.offlineModel.SearchTree(env.state, int(act), 1)
+	//fmt.Println(env.state, act)
+	//fmt.Println(nextStates, rewards, terminals, "\n")
+	env.state = nextStates[0] // only 1 neighbor
 	var done bool
-	if gammas[0] == 0 {
-		done = true
-	} else {
+	if terminals[0] == 0 {
 		done = false
+	} else {
+		done = true
 	}
-	return cp, rewards[0], done
+	//fmt.Println(env.state)
+	return env.state, rewards[0], done
 }
-
 
 //GetAttributes returns attributes for this environment.
 func (env *KnnModelEnv) GetAttributes() rlglue.Attributes {
