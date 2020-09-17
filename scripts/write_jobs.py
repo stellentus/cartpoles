5d91f8c15052e793315f846145104cdba116cd91
hwang-ua <han8@ualberta.ca>
2020-09-17 17:32:22 -0600

    update

--- scripts/write_jobs.py
+++ scripts/write_jobs.py
@@ -13,18 +13,15 @@ def write_job(sweep, run, config_list, prev=0, line_per_file=1):
 
 
 # write_job(1, 30, ["config/hyperparam/cartpole/offline_data/dqn/step10k_env/lockat_-0.1.json",
-#                   "config/hyperparam/cartpole/offline_data/dqn/step10k_env/lockat_baseline.json",
-#                   "config/hyperparam/cartpole/offline_data/dqn/step10k_env/lockat_halfbaseline.json",
-#                   "config/hyperparam/cartpole/offline_data/dqn/step10k_env/lockat_quarterbaseline.json",
-#                   "config/hyperparam/cartpole/offline_data/dqn/step10k_env/lockat_random.json",
 #                   "config/hyperparam/cartpole/offline_data/dqn/step1k_env/lockat_-0.1.json",
-#                   "config/hyperparam/cartpole/offline_data/dqn/step1k_env/lockat_baseline.json",
-#                   "config/hyperparam/cartpole/offline_data/dqn/step1k_env/lockat_halfbaseline.json",
-#                   "config/hyperparam/cartpole/offline_data/dqn/step1k_env/lockat_quarterbaseline.json",
-#                   "config/hyperparam/cartpole/offline_data/dqn/step1k_env/lockat_random.json"]
-#           , prev=0, line_per_file=30)
+#                   "config/hyperparam/cartpole/offline_data/dqn/step20k_env/lockat_-0.1.json",
+#                   "config/hyperparam/cartpole/offline_data/dqn/step20k_env/lockat_baseline.json",
+#                   "config/hyperparam/cartpole/offline_data/dqn/step20k_env/lockat_halfbaseline.json",
+#                   "config/hyperparam/cartpole/offline_data/dqn/step20k_env/lockat_quarterbaseline.json",
+#                   "config/hyperparam/cartpole/offline_data/dqn/step20k_env/lockat_random.json"]
+#           , prev=0, line_per_file=1)
 
-# write_job(5, 30, ["config/hyperparam/cartpole/offline_learning/dqn-adam/step10k_env/lockat_-0.1.json",
+# write_job(36, 30, ["config/hyperparam/cartpole/offline_learning/dqn-adam/step10k_env/lockat_-0.1.json",
 #                   "config/hyperparam/cartpole/offline_learning/dqn-adam/step10k_env/lockat_baseline.json",
 #                   "config/hyperparam/cartpole/offline_learning/dqn-adam/step10k_env/lockat_halfbaseline.json",
 #                   "config/hyperparam/cartpole/offline_learning/dqn-adam/step10k_env/lockat_quarterbaseline.json",
@@ -35,18 +32,15 @@ def write_job(sweep, run, config_list, prev=0, line_per_file=1):
 #                   "config/hyperparam/cartpole/offline_learning/dqn-adam/step1k_env/lockat_quarterbaseline.json",
 #                   "config/hyperparam/cartpole/offline_learning/dqn-adam/step1k_env/lockat_random.json",
 #                   ]
-#           , prev=0, line_per_file=40)
+#           , prev=0, line_per_file=120) # 12 h
 
-# write_job(5, 30, ["config/hyperparam/cartpole/online_learning/dqn-sgd/step50k/sweep_lr.json"]
-#           , prev=75, line_per_file=20)
+write_job(36, 30, ["config/hyperparam/cartpole/offline_learning/dqn-adam/step20k_env/lockat_-0.1.json",
+                  "config/hyperparam/cartpole/offline_learning/dqn-adam/step20k_env/lockat_baseline.json",
+                  "config/hyperparam/cartpole/offline_learning/dqn-adam/step20k_env/lockat_halfbaseline.json",
+                  "config/hyperparam/cartpole/offline_learning/dqn-adam/step20k_env/lockat_quarterbaseline.json",
+                  "config/hyperparam/cartpole/offline_learning/dqn-adam/step20k_env/lockat_random.json",
+                  ]
+          , prev=0, line_per_file=40)
 
-# write_job(5, 30, ["config/hyperparam/cartpole/offline_learning/esarsa-adam/step10k_env/lockat_-0.1.json",
-#                   "config/hyperparam/cartpole/offline_learning/esarsa-adam/step10k_env/lockat_baseline.json",
-#                   "config/hyperparam/cartpole/offline_learning/esarsa-adam/step10k_env/lockat_halfbaseline.json",
-#                   "config/hyperparam/cartpole/offline_learning/esarsa-adam/step10k_env/lockat_quarterbaseline.json",
-#                   "config/hyperparam/cartpole/offline_learning/esarsa-adam/step10k_env/lockat_random.json",
-#                   ]
-#           , prev=0, line_per_file=5)
-
-write_job(5, 30, ["config/hyperparam/cartpole/online_learning/esarsa-adam/step50k/sweep_lr.json"]
-          , prev=150, line_per_file=5)
+write_job(36, 30, ["config/hyperparam/cartpole/online_learning/dqn-adam/step50k/sweep_lr.json"]
+          , prev=180, line_per_file=120)
