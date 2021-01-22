09a07f22f10bb5474bcd5764c6780a442a423966
Archit Sakhadeo <gemsbound007@users.noreply.github.com>
2021-01-22 15:34:30 -0700

    3 -> 6 boxes plot

--- plot/box/acrobot_coverage.py
+++ plot/box/acrobot_coverage.py
@@ -13,15 +13,9 @@ def plot_generation(te, cms, title, ylim=None):
     # te_rank = ranking_allruns(te_data)["true"]
     te_data = te_data["true"]
 
-    # thrd10 = np.array([item[2] for item in percentile(te_rank, ranges[0][0], ranges[0][1])]).min()
-    # thrd20 = np.array([item[2] for item in percentile(te_rank, ranges[1][0], ranges[1][1])]).min()
-    # thrd30 = np.array([item[2] for item in percentile(te_rank, ranges[2][0], ranges[2][1])]).min()
-    thrd10 = percentile_avgeraged_run(te_data, ranges[0][1])
-    thrd20 = percentile_avgeraged_run(te_data, ranges[1][1])
-    thrd30 = percentile_avgeraged_run(te_data, ranges[2][1])
-    thrd40 = percentile_avgeraged_run(te_data, ranges[3][1])
-    thrd50 = percentile_avgeraged_run(te_data, ranges[4][1])
-    te_thrd = [thrd10, thrd20, thrd30, thrd40, thrd50]
+    te_thrd = []
+    for perc in ranges:
+        te_thrd.append(percentile_avgeraged_run(te_data, perc))
 
     cms_data = loading_pessimistic(cms)
     filtered = {}
@@ -29,19 +23,11 @@ def plot_generation(te, cms, title, ylim=None):
     for model in cms_data.keys():
         ranks = models_rank[model]
 
-        perc10 = percentile(ranks, ranges[0][0], ranges[0][1])
-        perc20 = percentile(ranks, ranges[1][0], ranges[1][1])
-        perc30 = percentile(ranks, ranges[2][0], ranges[2][1])
-        perc40 = percentile(ranks, ranges[3][0], ranges[3][1])
-        perc50 = percentile(ranks, ranges[4][0], ranges[4][1])
-
-        data10 = [te_data[item[0]][item[1]] for item in perc10]
-        data20 = [te_data[item[0]][item[1]] for item in perc20]
-        data30 = [te_data[item[0]][item[1]] for item in perc30]
-        data40 = [te_data[item[0]][item[1]] for item in perc40]
-        data50 = [te_data[item[0]][item[1]] for item in perc50]
-
-        filtered[model] = [data10, data20, data30, data40, data50]
+        filtered[model] = []
+        for perc in ranges:
+            target = percentile(ranks, perc)
+            data = [te_data[item[0]][item[1]] for item in target]
+            filtered[model].append(data)
 
     plot_boxs(filtered, te_thrd, labels, title, ylim=ylim)
 
@@ -96,8 +82,8 @@ def sweep_datasize():
     # plot_generation(te, eps03_cms, "../img/datasize_eps0.3")
     plot_generation(te, eps1_cms, "../img/datasize_eps1")
 
-ranges = [[0, 0.0001], [0.0001, 0.1], [0.1, 0.2], [0.2, 0.3], [0.3, 0.4]]
-labels = ["0%\n(top param)", "10%", "20%", "30%", "40%"]
+ranges = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+labels = ["0%\n(top param)", "10%", "20%", "30%", "40%", "50%"]
 #ranges = [[0, 0.3], [0.3, 0.7], [0.7, 1.0]]
 
 sweep_model()
