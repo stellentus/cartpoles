fix average trueperf

plot/box/utils_data.py
@@ -78,7 +78,13 @@ def percentile(ranked, perc):
 #     else:
 #         return filtered
 
-def percentile_avgeraged_run(data, perc):
+def percentile_avgeraged_run(group_by_param, perc):
+    # group_by_param = average_run(data)
+    sorted_group = sorting(group_by_param)
+    h = min(int(len(sorted_group) * perc), len(sorted_group)-1)
+    return sorted_group[h][1]
+
+def average_run(data):
     group_by_param = {}
     all_runs = list(data.keys())
     all_params = list(data[all_runs[0]].keys())
@@ -88,10 +94,7 @@ def percentile_avgeraged_run(data, perc):
             group_by_param[pk].append(data[rk][pk])
     for pk in all_params:
         group_by_param[pk] = np.array(group_by_param[pk]).mean()
-    sorted_group = sorting(group_by_param)
-    h = min(int(len(sorted_group) * perc), len(sorted_group)-1)
-    return sorted_group[h][1]
-
+    return group_by_param
 """
 input:
     path: ensemble_paths of one ensemble model [path_ens1, path_ens2, ...]