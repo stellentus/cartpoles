766a1712fe418a735f575b07859204f4db74dd4f
hwang-ua <han8@ualberta.ca>
2021-01-26 13:35:21 -0700

    old model test path

---
+++ plot/box/paths_model_test.py
@@ -0,0 +1,93 @@
+trueenv = ["../../data/hyperparam/cartpole/online_learning/esarsa-adam/step50k/sweep"]
+
+"""
+Baseline
+"""
+data10k_eps01_k5_p02_t0 = [
+    "../../data/hyperparam/cartpole/offline_learning/knn-ens-ep/k5/timeout0/esarsa/step10k_env/data_eps0.1/drop0.2/ensembleseed1",
+    "../../data/hyperparam/cartpole/offline_learning/knn-ens-ep/k5/timeout0/esarsa/step10k_env/data_eps0.1/drop0.2/ensembleseed2",
+    "../../data/hyperparam/cartpole/offline_learning/knn-ens-ep/k5/timeout0/esarsa/step10k_env/data_eps0.1/drop0.2/ensembleseed3",
+    "../../data/hyperparam/cartpole/offline_learning/knn-ens-ep/k5/timeout0/esarsa/step10k_env/data_eps0.1/drop0.2/ensembleseed4",
+    "../../data/hyperparam/cartpole/offline_learning/knn-ens-ep/k5/timeout0/esarsa/step10k_env/data_eps0.1/drop0.2/ensembleseed5",
+]
+"""
+Timeout
+"""
+timeout_t100 = [
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout100/ensembleseed1",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout100/ensembleseed2",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout100/ensembleseed3",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout100/ensembleseed4",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout100/ensembleseed5",
+]
+
+timeout_t200 = [
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout200/ensembleseed1",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout200/ensembleseed2",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout200/ensembleseed3",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout200/ensembleseed4",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout200/ensembleseed5",
+]
+
+timeout_t500 = [
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout500/ensembleseed1",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout500/ensembleseed2",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout500/ensembleseed3",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout500/ensembleseed4",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/timeout/timeout500/ensembleseed5",
+]
+
+"""
+start from furthest state
+"""
+startfurthes_t0 = [
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout0/ensembleseed1",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout0/ensembleseed2",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout0/ensembleseed3",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout0/ensembleseed4",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout0/ensembleseed5",
+]
+startfurthes_t200 = [
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout200/ensembleseed1",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout200/ensembleseed2",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout200/ensembleseed3",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout200/ensembleseed4",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/start/furthest/timeout200/ensembleseed5",
+]
+
+"""
+noisy data
+"""
+noisystate_n10 = [
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.1/ensembleseed1",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.1/ensembleseed2",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.1/ensembleseed3",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.1/ensembleseed4",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.1/ensembleseed5",
+]
+noisystate_n50 = [
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.5/ensembleseed1",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.5/ensembleseed2",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.5/ensembleseed3",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.5/ensembleseed4",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/noisydata/sNoise0.5/ensembleseed5",
+]
+
+"""
+transition to the furthest state
+"""
+transfurthest_t0 = [
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout0/ensembleseed1/",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout0/ensembleseed2/",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout0/ensembleseed3/",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout0/ensembleseed4/",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout0/ensembleseed5/",
+]
+
+transfurthest_t200 = [
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout200/ensembleseed1/",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout200/ensembleseed2/",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout200/ensembleseed3/",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout200/ensembleseed4/",
+    "../../data/hyperparam/cartpole/offline_learning/data_coverage_test/ens_k5_p0.2_data10k_eps0.1/furthesttrans/furthest_start/timeout200/ensembleseed5/",
+]
