6539405ef2b9354950a219b94d0a035c19f3ad1b
hwang-ua <han8@ualberta.ca>
2021-01-26 13:36:39 -0700

    clean old result paths. Ready to add paths for random start data

--- plot/box/cartpole_coverage.py
+++ plot/box/cartpole_coverage.py
@@ -8,85 +8,12 @@ from plot.box.paths_cartpole import *
 
 def sweep_model():
     cms = {
-        "k1_p0_t0": data10k_eps1_k1_p0_t0,
-        "k5_p02_t0": data10k_eps1_k5_p02_t0,
-        "k5_p02_t200": data10k_eps1_k5_p02_t200,
         # "k5_p02_t1000": data10k_eps03_k5_p02_t1000,
     }
     te = {"true": trueenv}
     plot_generation(te, cms, ranges, "../img/sweep_model")
 
-def sweep_coverage():
-    cms = {
-            "eps0": data10k_eps0_k5_p02_t0,
-            "eps0.1": data10k_eps01_k5_p02_t0,
-            "eps0.3": data10k_eps03_k5_p02_t0,
-            "eps1": data10k_eps1_k5_p02_t0,
-    }
-    te = {"true": trueenv}
-    plot_generation(te, cms, ranges, "../img/coverage_data10k")
 
-def sweep_datasize():
-    eps0_cms = {
-        "2.5k": data2d5k_eps0_k5_p02_t0,
-        "5k": data5k_eps0_k5_p02_t0,
-        "10k": data10k_eps0_k5_p02_t0,
-        "20k": data20k_eps0_k5_p02_t0,
-    }
-    eps01_cms = {
-        "2.5k": data2d5k_eps01_k5_p02_t0,
-        "5k": data5k_eps01_k5_p02_t0,
-        "10k": data10k_eps01_k5_p02_t0,
-        "20k": data20k_eps01_k5_p02_t0,
-    }
-    eps03_cms = {
-        "2.5k": data2d5k_eps03_k5_p02_t0,
-        "5k": data5k_eps03_k5_p02_t0,
-        "10k": data10k_eps03_k5_p02_t0,
-        "20k": data20k_eps03_k5_p02_t0,
-    }
-    eps1_cms = {
-        "2.5k": data2d5k_eps1_k5_p02_t0,
-        "5k": data5k_eps1_k5_p02_t0,
-        "10k": data10k_eps1_k5_p02_t0,
-        "20k": data20k_eps1_k5_p02_t0,
-    }
-    te = {"true": trueenv}
-
-    # plot_generation(te, eps0_cms, "../img/datasize_eps0")
-    # plot_generation(te, eps01_cms, "../img/datasize_eps0.1")
-    # plot_generation(te, eps03_cms, "../img/datasize_eps0.3")
-    plot_generation(te, eps1_cms, ranges, "../img/datasize_eps1")
-
-def discussion_plots():
-    eps0_10k = {
-        "cm": noisy50_data10k_eps0_k5_p02_t0
-    }
-    eps0_2d5k = {
-        "cm": noisy50_data2d5k_eps0_k5_p02_t0
-    }
-    eps01_10k = {
-        "cm": noisy50_data10k_eps01_k5_p02_t0
-    }
-    eps01_2d5k = {
-        "cm": noisy50_data2d5k_eps01_k5_p02_t0
-    }
-    te = {"true": trueenv}
-
-
-    plot_generation(te, eps01_10k, ranges, "../img/eps01_data10k")
-    plot_generation(te, eps0_10k, ranges, "../img/eps0_data10k")
-    plot_generation(te, eps01_2d5k, ranges, "../img/eps01_data2d5k")
-    plot_generation(te, eps0_2d5k, ranges, "../img/eps0_data2d5k")
-
-
-
-# ranges = [0, 0.1, 0.2, 0.3]
-# ranges = [0, 0.2, 0.4, 0.6]
 ranges = [0, 0.1, 0.5]
 
 # sweep_model()
-# sweep_coverage()
-# sweep_datasize()
-
-marthas_plots()
