dc5c6132227a489b58dd5d220c6b1d92fe0a94e8
Archit Sakhadeo <gemsbound007@users.noreply.github.com>
2021-01-29 21:06:05 -0700

    acrobot positive scale box plots, random generator

--- plot/box/paths_final.py
+++ plot/box/paths_final.py
@@ -1,4 +1,4 @@
-ac_true_env = ["../../data/hyperparam_randomStart/acrobot/online_learning/esarsa/step50k/gridsearch_realenv/"]
+ac_true_env = ["../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/online_learning/esarsa/step50k/gridsearch_realenv/"]
 
 # ac_cm = [
 #     "../../data/hyperparam_randomStart/acrobot/offline_learning/knn-ensemble/k3/esarsa/step10k/drop0.2/ensembleseed1/optimalfixed_eps0/",
@@ -17,10 +17,10 @@ ac_cm = [
 ]
 
 ac_fqi = [
-    "../../data/hyperparam_randomStart/acrobot/offline_learning/fqi/fqi-adam/alpha_hidden_epsilon/step10k_env/data_eps0/lockat_baseline_online/"
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/fqi/fqi-adam/alpha_hidden_epsilon/step10k_env/data_eps0/lockat_baseline_online/"
 ]
 
-ac_rnd = [19, 4, 3, 4, 31, 30, 8, 3, 27, 21, 13, 10, 16, 0, 15, 33, 30, 22, 5, 19, 25, 19, 32, 9, 1, 5, 5, 32, 15, 16, 24, 32, 21, 17, 18, 16]
+ac_rnd = [2, 0, 29, 10, 28, 30, 0, 29, 14, 24, 9, 8, 29, 11, 28, 9, 7, 5, 6, 8, 9, 7, 9, 3, 21, 2, 8, 21, 6, 20, 2, 16]
 # ac_rnd = [1, 2, 29, 30, 24, 28, 9, 21, 18, 11, 10, 20, 3, 14, 8, 13, 24, 19, 8, 1, 25, 0, 8, 18, 21, 31, 21, 22, 1, 19, 22, 13]
 
 """
@@ -54,6 +54,27 @@ trueStart_farTrans_time1000 = [
 cpn1_fqi = [
     "../../data/hyperparam_randomStart/cartpole-noisy-action/noise_1perc/offline_learning/fqi/fqi-adam/alpha_hidden_epsilon/step10k_env/data_eps0/lockat_baseline_online/"
 ]
+
+
+
+AcrobotdistantStart_regularTrans_timeout200 = [
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout200_distantStart_regularTrans/esarsa/step10k/drop0.2/ensembleseed1/optimalfixed_eps0/",
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout200_distantStart_regularTrans/esarsa/step10k/drop0.2/ensembleseed2/optimalfixed_eps0/",
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout200_distantStart_regularTrans/esarsa/step10k/drop0.2/ensembleseed3/optimalfixed_eps0/",
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout200_distantStart_regularTrans/esarsa/step10k/drop0.2/ensembleseed4/optimalfixed_eps0/",
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout200_distantStart_regularTrans/esarsa/step10k/drop0.2/ensembleseed5/optimalfixed_eps0/"
+]
+
+AcrobottrueStart_adversarialTrans_timeout1000 = [
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout1000_trueStart_adversarialTrans/esarsa/step10k/drop0.2/ensembleseed1/optimalfixed_eps0/",
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout1000_trueStart_adversarialTrans/esarsa/step10k/drop0.2/ensembleseed2/optimalfixed_eps0/",
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout1000_trueStart_adversarialTrans/esarsa/step10k/drop0.2/ensembleseed3/optimalfixed_eps0/",
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout1000_trueStart_adversarialTrans/esarsa/step10k/drop0.2/ensembleseed4/optimalfixed_eps0/",
+    "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout1000_trueStart_adversarialTrans/esarsa/step10k/drop0.2/ensembleseed5/optimalfixed_eps0/",
+]
+
+
+
 
 # cpn1_rnd = [18, 12, 31, 20, 3, 27, 20, 26, 22, 29, 16, 30, 35, 33, 23, 21, 14, 5, 0, 19, 21, 23, 32, 5, 17, 29, 21, 2, 15, 28, 23, 35, 25, 20, 14, 20]
 cpn1_rnd = [1, 2, 29, 30, 24, 28, 9, 21, 18, 11, 10, 20, 3, 14, 8, 13, 24, 19, 8, 1, 25, 0, 8, 18, 21, 31, 21, 22, 1, 19, 22, 13]
--- plot/box/random_generator.go
+++ plot/box/random_generator.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	seed := int64(1)//int64(time.Now().UnixNano())
+	seed := int64(1) //int64(time.Now().UnixNano())
 	num_samples := 32
 	var listOfValues []int64
 	for i := 0; i < num_samples; i++ {
-		seed += int64(i)
+		seed += int64(1)
 		rand.Seed(seed)
 		listOfValues = append(listOfValues, int64(rand.Intn(num_samples)))
 	}
