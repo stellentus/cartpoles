cbfbd1dda89c3f5a9609c467bd9612cfa51edb9e
hwang-ua <han8@ualberta.ca>
2021-01-30 16:27:18 -0700

    scatter plot

--- plot/box/cartpole-noisy-a_coverage.py
+++ plot/box/cartpole-noisy-a_coverage.py
@@ -5,35 +5,52 @@ sys.path.insert(0, cwd+'/../..')
 from plot.box.utils_plot import *
 from plot.box.paths_cartpoleNoisyA import *
 
-def noise1():
-    eps0 = {
-        "timeout1000": ns1_timeout1000,
-    }
-    te = {"true": ns1_true_env}
-
-    plot_generation(te, eps0, ranges, "../img/ns1_eps0")
-
-def sweep_model():
+def sweep_model_rs():
 
     cms = {
-        "RS_distStart_adversarialTrans_t200": RS_farStart_farTrans_t200,
-        "RS_trueStart_adversarialTrans_t1000": RS_trueStart_farTrans_t1000,
+        "trueStart_adversarialTrans_t1000": RS_trueStart_farTrans_time1000,
+        "distStart_adversarialTrans_t200": RS_distStart_farTrans_time200,
+        "distStart_closeTrans_t200": RS_distStart_closeTrans_time200,
     }
-    te = {"true": RS_ns1_true_env}
-    plot_generation(te, cms, ranges, "../img/sweep_model")
+    te = {"true": cpn1_true_env}
+    plot_generation(te, cms, ranges, "../img/sweep_model_RS")
 
     """
     no random restart
     """
-    # cms = {
-    #     "trueStart_adversariaTrans_t1000": trueStart_farTrans_time1000,
-    #     # "distStart_adversariaTrans_t200": distStart_farTrans_time200,
-    #     "distStart_closeTrans_t200": distStart_closeTrans_time200,
-    # }
-    # te = {"true": cpn1_true_env}
+def sweep_model():
+    cms = {
+        "trueStart_adversariaTrans_t1000": trueStart_farTrans_time1000,
+        # "distStart_adversariaTrans_t200": distStart_farTrans_time200,
+        # "distStart_closeTrans_t200": distStart_closeTrans_time200,
+        "trueStart_adversarialTrans_t1000": trueStart_farTrans_time1000, #
+        "trueStart_adversarialTrans_t0": trueStart_farTrans_time0,
+        "noAdversarial_t1000": trueStart_closeTrans_time1000, #
+        "noAdversarial_t0": trueStart_closeTrans_time0,
+        "noEnsemble_t1000": trueStart_noEnsemble_time1000, #
+        "noEnsemble_t0": trueStart_noEnsemble_time0,
+    }
+    te = {"true": cpn1_true_env}
+    plot_generation(te, cms, ranges, "../img/sweep_model_noRS")
 
+def check_run():
+    cms = {
+        "trueStart_adversarialTrans_t1000": trueStart_farTrans_time1000, #
+        # "distStart_adversariaTrans_t200": distStart_farTrans_time200,
+        # "distStart_closeTrans_t200": distStart_closeTrans_time200,
+
+        "trueStart_adversarialTrans_t0": trueStart_farTrans_time0,
+        # "noAdversarial_t1000": trueStart_closeTrans_time1000, #
+        # # "noAdversarial_t0": trueStart_closeTrans_time0,
+        # "noEnsemble_t1000": trueStart_noEnsemble_time1000, #
+        # # "noEnsemble_t0": trueStart_noEnsemble_time0,
+    }
+    te = {"true": cpn1_true_env}
+    plot_each_run(te, cms, "../img/check_run_noRS")
 
 if __name__ == '__main__':
-    ranges = [0, 0.1, 0.2, 0.5, 0.7, 0.9]
+    ranges = [0, 0.05, 0.1, 0.2, 0.5, 0.7, 0.9]
     # noise1()
-    sweep_model()
+    # sweep_model_rs()
+    # sweep_model()
+    check_run()
--- plot/box/paths_final.py
+++ plot/box/paths_final.py
@@ -27,7 +27,7 @@ cartpole action noise 1%
 """
 cpn1_true_env = ["../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/online_learning/esarsa-adam/step50k/sweep/"]
 
-# compare
+# Without random start
 distStart_farTrans_time200 = [
     "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/farStart/farTrans/k3/timeout200/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed1",
     "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/farStart/farTrans/k3/timeout200/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed2",
@@ -49,9 +49,27 @@ trueStart_farTrans_time1000 = [
      "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed4",
      "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed5",
 ]
-
 cpn1_fqi = [
     "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/fqi/fqi-adam/alpha_hidden_epsilon/step10k_env/data_eps0/lockat_baseline_online/"
+]
+
+# With random start
+RS_distStart_closeTrans_time200 = [
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/farStart/closeTrans/k3/timeout200/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed1",
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/farStart/closeTrans/k3/timeout200/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed2",
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/farStart/closeTrans/k3/timeout200/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed3",
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/farStart/closeTrans/k3/timeout200/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed4",
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/farStart/closeTrans/k3/timeout200/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed5",
+]
+RS_trueStart_farTrans_time1000 = [
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed1",
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed2",
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed3",
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed4",
+    "../../data/hyperparam_rs_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed5",
+]
+RS_cpn1_fqi = [
+    "../../data/hyperparam_randomStart/cartpole-noisy-action/noise_1perc/offline_learning/fqi/fqi-adam/alpha_hidden_epsilon/step10k_env/data_eps0/lockat_baseline_online/"
 ]
 
 cpn1_rnd = [2, 0, 29, 10, 28, 30, 0, 29, 14, 24, 9, 8, 29, 11, 28, 9, 7, 5, 6, 8, 9, 7, 9, 3, 21, 2, 8, 21, 6, 20, 2, 16]
@@ -73,4 +91,36 @@ AcrobottrueStart_adversarialTrans_timeout1000 = [
     "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout1000_trueStart_adversarialTrans/esarsa/step10k/drop0.2/ensembleseed3/optimalfixed_eps0/",
     "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout1000_trueStart_adversarialTrans/esarsa/step10k/drop0.2/ensembleseed4/optimalfixed_eps0/",
     "../../../../../../Downloads/data_timeoutsAndtransitions_acrobot/acrobot/offline_learning/knn-ensemble/k3_50k_timeout1000_trueStart_adversarialTrans/esarsa/step10k/drop0.2/ensembleseed5/optimalfixed_eps0/",
+]
+
+
+"""
+Ablation study - Cartpole
+"""
+trueStart_farTrans_time0 = [
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed1",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed2",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed3",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed4",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/farTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed5",
+]
+trueStart_closeTrans_time1000 = [
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed1",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed2",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed3",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed4",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout1000/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed5",
+]
+trueStart_closeTrans_time0 = [
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed1",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed2",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed3",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed4",
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k3/timeout0/esarsa/step10k_env/data_eps0/drop0.2/ensembleseed5",
+]
+trueStart_noEnsemble_time1000 = [
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k1/timeout1000/esarsa/step10k_env/data_eps0/drop0/ensembleseed0",
+]
+trueStart_noEnsemble_time0 = [
+    "../../data/hyperparam_v1/cartpole-noisy-action/noise_1perc/offline_learning/knn-ens/randomInit/closeTrans/k1/timeout0/esarsa/step10k_env/data_eps0/drop0/ensembleseed0",
 ]
