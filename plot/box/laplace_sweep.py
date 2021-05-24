4cfaa23c700dfa24bfb02497875ee86c83ba2ffd
hwang-ua <han8@ualberta.ca>
2021-05-24 10:05:02 -0600

    plotting

--- plot/box/laplace_sweep.py
+++ plot/box/laplace_sweep.py
@@ -10,47 +10,34 @@ def env_param_eval(path, eval_key):
     res_dict = loading_info_all(path, eval_key=eval_key)
     print(dict(sorted(res_dict.items(), key=lambda item: item[1],reverse=True)))
 
-# print("\nPuddlerand laplace test0")
-# env_param_eval(pdrand_laplace_test0, "Averaged improvement = ")
-# print("\nPuddlerand laplace test1")
-# env_param_eval(pdrand_laplace_test1, "Dynamic awareness = ")
-#
 # print("\nAcrobot laplace test0")
 # env_param_eval(acrobot_laplace_test0, "Averaged improvement = ")
-# print("\nAcrobot laplace test1")
-# env_param_eval(acrobot_laplace_test1, "Dynamic awareness = ")
+print("\nAcrobot laplace test1")
+env_param_eval(acrobot_laplace_test1, "Dynamic awareness = ")
 
-# print("\nAcrobot network")
-# env_param_eval(acrobot_network, "Averaged loss = ")
+# print("\nPuddlerand laplace test0")
+# env_param_eval(pdrand_laplace_test0, "Averaged improvement = ")
+print("\nPuddlerand laplace test1")
+env_param_eval(pdrand_laplace_test1, "Dynamic awareness = ")
 
-'''
 print("\nAcrobot network scaled")
 env_param_eval(acrobot_scale_network, "Averaged loss = ")
 
-print("\nAcrobot network laplace")
-env_param_eval(acrobot_laplace_network, "Averaged loss = ")
-
-print("\nAcrobot network scaled laplace")
-env_param_eval(acrobot_scale_laplace_network, "Averaged loss = ")
-
-# print("\nPuddlerand network")
-# env_param_eval(pdrand_network, "Averaged loss = ")
+# print("\nAcrobot network scaled laplace")
+# env_param_eval(acrobot_scale_laplace_network, "Averaged loss = ")
 
 print("\nPuddlerand network scaled")
 env_param_eval(pdrand_scale_network, "Averaged loss = ")
 
-print("\nPuddlerand network laplace")
-env_param_eval(pdrand_laplace_network, "Averaged loss = ")
+# print("\nPuddlerand network scaled laplace")
+# env_param_eval(pdrand_scale_laplace_network, "Averaged loss = ")
 
-print("\nPuddlerand network scaled laplace")
-env_param_eval(pdrand_scale_laplace_network, "Averaged loss = ")
-'''
 
-print("\nCartpole Laplace Optimal\n")
-env_param_eval(cartpole_laplace_test1_optimal, "Dynamic awareness = ")
-
-print("\nCartpole Laplace Suboptimal\n")
-env_param_eval(cartpole_laplace_test1_suboptimal, "Dynamic awareness = ")
-
-print("\nCartpole Laplace Random\n")
-env_param_eval(cartpole_laplace_test1_random, "Dynamic awareness = ")
+# print("\nCartpole Laplace Optimal\n")
+# env_param_eval(cartpole_laplace_test1_optimal, "Dynamic awareness = ")
+#
+# print("\nCartpole Laplace Suboptimal\n")
+# env_param_eval(cartpole_laplace_test1_suboptimal, "Dynamic awareness = ")
+#
+# print("\nCartpole Laplace Random\n")
+# env_param_eval(cartpole_laplace_test1_random, "Dynamic awareness = ")
